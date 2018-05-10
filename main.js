94a7ea3b967f1f25df10cbaf6a671781979a006f
Thomas Gardiner <thomgardiner@gmail.com>
2018-05-10 10:16:55 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ const call = function(){
         "app_id"          : "ce71ec90",
         "app_key"         : "205f9b7807db5aa99dfb98a98fb886aa"
     };
-    var image  = "https://media.kairos.com/liz.jpg
- https://media.kairos.com/liz.jpg
- ";
+    var image  = "https://media.kairos.com/liz.jpg";
     var url = "https://api.kairos.com/v2/media?source=" + image + "&landmarks=1";
     // make request
     $.ajax(url, {
